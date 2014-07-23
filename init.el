9f10a6a2f8fe0cd50c470001fd37b7467d8fea8f
Danny Gratzer <jozefg@cmu.edu>
2014-07-23 17:21:01 -0400

    some shuffling in init.el

--- init.el
+++ init.el
@@ -3,16 +3,16 @@
 (require 'init-elpa)     ; Packaging
 (require 'init-haskell)  ; Haskell
 (require 'init-python)   ; Python
-(require 'init-markdown) ; Configure markdown
-(require 'init-backups)  ; Configure how emacs autosaves/backups work
 (require 'init-coq)      ; Proof General & Coq
 (require 'init-agda)     ; Setup Agda paths & highlighting
 (require 'init-idris)    ; Setup Idris + Helm integration
+(require 'init-markdown) ; Configure markdown
+(require 'init-journal)  ; Get org-journal properly setup and integrated
+(require 'init-backups)  ; Configure how emacs autosaves/backups work
 (require 'init-my-keys)  ; The minor mode for all my key shortcuts
 (require 'init-mail)     ; Setup mu4e and smtpmail for gmail
 (require 'init-font)     ; Source Code Pro
 (require 'init-helm)     ; Get Helm properly integrated
-(require 'init-journal)  ; Get org-journal properly setup and integrated
 (require 'init-colors)   ; Pretty color theme
 
 ;; Packages
