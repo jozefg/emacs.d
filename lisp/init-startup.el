b2af104d07d3e9e8a400682fbb5304b7cf86bfae
Danny Gratzer <jozefg@cmu.edu>
2014-07-27 22:56:46 -0400

    None of this fancy shamshy gui stuff

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -1,3 +1,7 @@
 (setq inhibit-startup-message t)   ; No startup banner
 (setq initial-scratch-message nil) ; No message in scratch buffer
+(tool-bar-mode -1)                 ; No toolbars
+(toggle-scroll-bar -1)             ; No scroll bar
+(menu-bar-mode -1)                 ; No menu bar
+
 (provide 'init-startup)
