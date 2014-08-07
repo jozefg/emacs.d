abcf92d7a29cfdc8d4189eb2c5f5b10a5823703a
Danny Gratzer <jozefg@cmu.edu>
2014-08-07 10:12:25 -0400

    Updated latex mode with nicer configuration

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -1,2 +1,17 @@
-(require-package 'auctex)
+;; auctex is under the name auctex on elpa but provides
+;; the symbol tex. No clue why.. but that's why I've
+;; inlined require-package here.
+(when (not (require 'tex nil t))
+    (package-install 'auctex)
+    (require 'tex))
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq TeX-PDF-mode t)
+
+;; Previews are nice
 (require-package 'latex-preview-pane)
+(provide 'init-latex)
