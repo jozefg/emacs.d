c8b6ec6bf147c79b868990f7b244e5e5429f82a2
Danny Gratzer <jozefg@cmu.edu>
2014-08-19 14:26:18 -0400

    Added my name and email

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -1,3 +1,6 @@
+(setq user-full-name "Danny Gratzer")
+(setq user-mail-address "jozefg@cmu.edu")
+
 (setq inhibit-startup-message t)   ; No startup banner
 (setq initial-scratch-message nil) ; No message in scratch buffer
 (tool-bar-mode -1)                 ; No toolbars
