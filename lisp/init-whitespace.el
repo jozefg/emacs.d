228460b627eab4c224fa92188000a5eaa015b528
Danny Gratzer <jozefg@cmu.edu>
2014-08-26 18:01:02 -0400

    Added face to whitespace config for pretty pink highlighting

--- lisp/init-whitespace.el
+++ lisp/init-whitespace.el
@@ -1,8 +1,8 @@
  (require 'whitespace)
 
-(setq whitespace-line-column 80) ; This is the default value, but I tweak it often
-(setq whitespace-style '(tabs lines-tail))
+(setq whitespace-line-column 80) ; This is the default value, but I tweak a lot
+(setq whitespace-style '(face tabs lines-tail))
 
-(global-whitespace-mode t) ; Judgement everywhere.
+(global-whitespace-mode 1) ; Judgement everywhere.
 
 (provide 'init-whitespace)
