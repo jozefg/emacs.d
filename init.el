dbd8e9afba2d423faf5a115901731c1c63840de0
Danny Gratzer <jozefg@cmu.edu>
2014-08-26 17:57:59 -0400

    Added some config for whitespace-mode

--- init.el
+++ init.el
@@ -1,31 +1,32 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-(require 'init-elpa)     ; Packaging
-(require 'init-haskell)  ; Haskell
-(require 'init-python)   ; Python
-(require 'init-coq)      ; Proof General & Coq
-(require 'init-ocaml)    ; Nicer key bindings for Tuareg
-(require 'init-agda)     ; Setup Agda paths & highlighting
-(require 'init-idris)    ; Setup Idris + Helm integration
-(require 'init-js)       ; js-comint mode
-(require 'init-markdown) ; Configure markdown
-(require 'init-latex)    ; Configure latex, preview pane and other niceness
-(require 'init-journal)  ; Get org-journal properly setup and integrated
-(require 'init-org)      ; Setup org mode with nicities
-(require 'init-backups)  ; Configure how emacs autosaves/backups work
-(require 'init-util-fns) ; Some useful functions I bind to keys
-(require 'init-my-keys)  ; The minor mode for all my key shortcuts
-(require 'init-hippie)   ; Configure hippie
-(require 'init-mail)     ; Setup mu4e and smtpmail for gmail
-(require 'init-eshell)   ; Setup eshell
-(require 'init-font)     ; Source Code Pro
-(require 'init-vcs)      ; Initialization and configuration for all things VCS
-(require 'init-helm)     ; Get Helm properly integrated
-(require 'init-colors)   ; Pretty color theme
-(require 'init-startup)  ; Configure startup message and stuff like that
-(require 'init-parens)   ; Pretty parens
-(require 'init-tabs)     ; Down with tabs
-(require 'init-gpg)      ; For encrypting stuffs
-(require 'init-delete)   ; Hungry delete mode
-(require 'init-random)   ; Random packages with no configuration
-(require 'init-smiles)   ; Just stupid fun stuff
+(require 'init-elpa)       ; Packaging
+(require 'init-haskell)    ; Haskell
+(require 'init-python)     ; Python
+(require 'init-coq)        ; Proof General & Coq
+(require 'init-ocaml)      ; Nicer key bindings for Tuareg
+(require 'init-agda)       ; Setup Agda paths & highlighting
+(require 'init-idris)      ; Setup Idris + Helm integration
+(require 'init-js)         ; js-comint mode
+(require 'init-markdown)   ; Configure markdown
+(require 'init-latex)      ; Configure latex, preview pane and other niceness
+(require 'init-journal)    ; Get org-journal properly setup and integrated
+(require 'init-org)        ; Setup org mode with nicities
+(require 'init-backups)    ; Configure how emacs autosaves/backups work
+(require 'init-util-fns)   ; Some useful functions I bind to keys
+(require 'init-my-keys)    ; The minor mode for all my key shortcuts
+(require 'init-hippie)     ; Configure hippie
+(require 'init-mail)       ; Setup mu4e and smtpmail for gmail
+(require 'init-eshell)     ; Setup eshell
+(require 'init-font)       ; Source Code Pro
+(require 'init-vcs)        ; Initialization and configuration for all things VCS
+(require 'init-helm)       ; Get Helm properly integrated
+(require 'init-colors)     ; Pretty color theme
+(require 'init-startup)    ; Configure startup message and stuff like that
+(require 'init-parens)     ; Pretty parens
+(require 'init-tabs)       ; Down with tabs
+(require 'init-whitespace) ; Judgemental highlighting!
+(require 'init-gpg)        ; For encrypting stuffs
+(require 'init-delete)     ; Hungry delete mode
+(require 'init-random)     ; Random packages with no configuration
+(require 'init-smiles)     ; Just stupid fun stuff
