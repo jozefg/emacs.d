4a7f11cc5064b45bc46520cf0939c124efb1872f
Danny Gratzer <jozefg@cmu.edu>
2014-08-28 08:39:08 -0400

    Merlin mode for OCaml with less suck

--- README.md
+++ README.md
@@ -11,5 +11,6 @@ This isn't completely dependency free. It expects
  - ProofGeneral   for proof general in init-coq, it's not packaged with elpa yet
  - Ag             for ag.el
  - Mu4e           for mu4e, this also depends on mu and offlineimap
+ - Merlin + Opam  for merlin-mode, autocompletion for ocaml
 
 All of these are avaible with a good package manager (apt-get, pacman)
--- lisp/init-ocaml.el
+++ lisp/init-ocaml.el
@@ -1,4 +1,21 @@
 (require 'tuareg)
 (define-key tuareg-mode-map (kbd "C-c C-l") 'tuareg-eval-buffer)
 
+;; Merlin provides autocomplete, but it's an OCaml lib, so this
+;; depends on it.
+(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+(require 'merlin)
+
+;; Always turn on Merlin
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+;; Enable auto-complete
+(setq merlin-use-auto-complete-mode 'easy)
+;; Use opam switch to lookup ocamlmerlin binary
+(setq merlin-command 'opam)
+
+;; Instead of hippy expand, use Merlin!
+(define-key tuareg-mode-map (kbd "C-;") 'completion-at-point)
+
+
 (provide 'init-ocaml)
