de83e996debce43acb0d53417169bea6e4b36a2f
Danny Gratzer <danny.gratzer@gmail.com>
2014-08-31 11:44:00 -0400

    Prettified some comments

--- lisp/init-colors.el
+++ lisp/init-colors.el
@@ -5,11 +5,11 @@
 (color-theme-initialize)
 
 ;; Customize solarize
-(setq solarized-distinct-fringe-background t) ;; make the fringe stand out from the background
-(setq solarized-high-contrast-mode-line t) ;; make the modeline high contrast
-(setq solarized-use-less-bold t) ;; Use less bolding
-(setq solarized-use-more-italic t) ;; Use more italics
-(setq solarized-emphasize-indicators nil) ;; Use less colors for indicators
+(setq solarized-distinct-fringe-background t) ;; make the fringe stand out
+(setq solarized-high-contrast-mode-line t)    ;; make the modeline high contrast
+(setq solarized-use-less-bold t)              ;; Use less bolding
+(setq solarized-use-more-italic t)            ;; Use more italics
+(setq solarized-emphasize-indicators nil)     ;; Use less colors for indicators
 
 
 (load-theme 'solarized-dark t)
