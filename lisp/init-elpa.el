88ff8e8101cca8dfa0b3c28ae9e49db6102e7092
Danny Gratzer <danny.gratzer@gmail.com>
2014-09-03 18:01:56 -0400

    Added forgotten quote

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -4,7 +4,7 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
-               ("marmalade" . "http://marmalade-repo.org/packages/"))
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
