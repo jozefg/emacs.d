ba2fb2306fd93ebf95116e589362a4b13a2858de
Danny Gratzer <danny.gratzer@gmail.com>
2014-09-03 16:40:11 -0400

    Added some support for Cargo and flymake for rust

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,9 +1,21 @@
 (require-package 'rust-mode)
+(require-package 'flymake-easy)
+(require-package 'flymake-rust)
+
+(add-hook 'rust-mode-hook 'flymake-rust-load)
+
 
 (defun rust-compile (buffer)
   (interactive (list (buffer-name)))
   (compile (concat "rustc " buffer)))
 
-(define-key rust-mode-map (kbd "C-c C-c") 'rust-compile)
+(defun cargo-compile ()
+  (interactive "")
+  (compile "cargo build"))
+
+
+
+(define-key rust-mode-map (kbd "C-c C-l") 'rust-compile)
+(define-key rust-mode-map (kbd "C-c C-c") 'cargo-compile)
 
 (provide 'init-rust)
