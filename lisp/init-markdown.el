c99f5e10897b7d19d1aa5a0cfd82c1d697606f52
Danny Gratzer <danny.gratzer@gmail.com>
2014-09-03 18:02:11 -0400

    Make sure to load markdown-mode for .md files

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,5 +3,6 @@
 (add-hook 'markdown-mode-hook 'auto-fill-mode)
 (add-hook 'markdown-mode-hook 'pandoc-mode)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'"       . markdown-mode))
 
 (provide 'init-markdown)
