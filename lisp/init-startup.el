17c408f76bb4d0d0b241466f0f18dce9c23b8a08
Danny Gratzer <jozefg@cmu.edu>
2014-10-02 14:54:58 -0400

    Prettier modeline

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -7,4 +7,13 @@
 (toggle-scroll-bar -1)             ; No scroll bar
 (menu-bar-mode -1)                 ; No menu bar
 
+;; A pretty modeline is worth every character
+(require-package 'dash) ; Implicit dependency for sml
+(require-package 'smart-mode-line)
+
+(setq sml/no-confirm-load-theme t) ; Shhhh
+(sml/apply-theme 'respectful)
+
+(sml/setup) ; Pretty modeline
+
 (provide 'init-startup)
