35f405511c7155820455c1cc86f7ab53d4e4752a
Danny Gratzer <jozefg@cmu.edu>
2014-10-07 23:51:06 -0400

    Added a new function to toggle monky display

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -1,4 +1,10 @@
 (require-package 'monky)
 (require-package 'magit)
 
+(defun toggle-monky ()
+  (interactive)
+  (let ((buf (monky-find-status-buffer)))
+    (if buf (kill-buffer buf)
+      (monky-status))))
+
 (provide 'init-vcs)
