8695bd8eb000aadd8ab538b91d780a8fa29ee2dd
Danny Gratzer <jozefg@cmu.edu>
2014-10-23 17:26:15 -0400

    melpa.milkbox is now just melpa

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,7 +1,7 @@
 ;; package-install
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.net/packages/") t)
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 
