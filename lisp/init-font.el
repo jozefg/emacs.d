50a443708adcbc72789fde8a9beea326cad796ea
Danny Gratzer <jozefg@cmu.edu>
2014-10-28 10:57:56 -0400

    Slightly decreased font size

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -1,7 +1,7 @@
 ;; Use source pro
 (when (window-system)
   (set-frame-font "Source Code Pro")
-  (set-face-attribute 'default nil :font "Source Code Pro" :height 130)
+  (set-face-attribute 'default nil :font "Source Code Pro" :height 120)
   (set-face-font 'default "Source Code Pro"))
 
 ;; Helpful function to blow up font for presentations
