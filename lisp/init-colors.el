911646db6e3f797c0074aceddfb37d5353bb6e77
Danny Gratzer <jozefg@cmu.edu>
2014-10-28 10:57:46 -0400

    Theme now only shows up in graphical systems

--- lisp/init-colors.el
+++ lisp/init-colors.el
@@ -16,4 +16,12 @@
 ;(load-theme 'solarized-dark t)
 (load-theme 'monokai t)
 
+;; If we're living in th eterminal, we really don't want
+;; a color theme.
+(defun on-after-init ()
+  (unless (display-graphic-p (selected-frame))
+    (set-face-background 'default "unspecified-bg" (selected-frame))))
+
+(add-hook 'window-setup-hook 'on-after-init)
+
 (provide 'init-colors)
