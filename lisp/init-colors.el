8d5a17c13a19b4aff0573463200946a498d8ba9b
Danny Gratzer <jozefg@cmu.edu>
2014-11-03 00:46:13 -0500

    Removed some unnused themes from init-colors.el, we have vcs for that

--- lisp/init-colors.el
+++ lisp/init-colors.el
@@ -1,20 +1,10 @@
 (require-package 'color-theme)
-(require-package 'zenburn-theme)
-(require-package 'solarized-theme)
 (require-package 'monokai-theme)
 (require-package 'moe-theme)
 
 (setq color-theme-is-global t)
 (color-theme-initialize)
 
-;; Customize solarize
-(setq solarized-distinct-fringe-background t) ;; make the fringe stand out
-(setq solarized-high-contrast-mode-line t)    ;; make the modeline high contrast
-(setq solarized-use-less-bold t)              ;; Use less bolding
-(setq solarized-use-more-italic t)            ;; Use more italics
-(setq solarized-emphasize-indicators nil)     ;; Use less colors for indicators
-
-;(load-theme 'solarized-dark t)
 ;(load-theme 'monokai t)
 (load-theme 'moe-dark t)
 
