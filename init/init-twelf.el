8b97054b9c2e60c9869deb6c9083ecc658a5838c
Danny Gratzer <jozefg@cmu.edu>
2015-03-13 17:37:51 -0400

    Added useful functions for init-twelf

--- init/init-twelf.el
+++ init/init-twelf.el
@@ -1,4 +1,8 @@
 (setq twelf-root "~/twelf/")
 (load (concat twelf-root "emacs/twelf-init.el"))
 
+(defun twelf-toggle-unsafe ()
+  (interactive)
+  (setq twelf-unsafe (not twelf-unsafe)))
+
 (provide 'init-twelf)
