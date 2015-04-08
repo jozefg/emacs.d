cf4d6624deee227304a307086e855a60df5fd933
Danny Gratzer <jozefg@cmu.edu>
2015-04-08 12:05:55 -0400

    Added deleted configuratio for proof general

--- init/init-coq.el
+++ init/init-coq.el
@@ -1,7 +1,9 @@
 (setq auto-mode-alist (cons '("\\.v$" . coq-mode) auto-mode-alist))
 (autoload 'coq-mode "coq" "Major mode for editing Coq vernacular." t)
 
-(add-hook 'coq-mode-hook 'company-coq-initialize)
+(load-file "/usr/share/emacs24/site-lisp/proofgeneral/generic/proof-site.el")
+(setq coq-prog-name "/usr/local/bin/coqtop")
+
 
 (custom-set-variables
   '(coq-compile-before-require nil)
--- init/init-elisp.el
+++ init/init-elisp.el
@@ -1,1 +1,3 @@
 (require-package 'flycheck-package)
+
+(provide 'init-elisp)
