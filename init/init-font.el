11e893eb61c3e434e516674efbffd8dc61fb7765
Danny Gratzer <danny.gratzer@gmail.com>
2015-04-22 20:17:33 -0400

    Fixed init-font to not fail when launched as daemon

--- init/init-font.el
+++ init/init-font.el
@@ -1,11 +1,4 @@
-(defun make-font-not-awful (x)
-  ;; Use source pro
-  (when (window-system)
-    (set-frame-font "Source Code Pro")
-    (set-face-attribute 'default nil :font "Source Code Pro" :height 120)
-    (set-face-font 'default "Source Code Pro")))
-
-(push 'make-font-not-awful after-make-frame-functions)
+(add-to-list 'default-frame-alist '(font . "Source Code Pro"))
 
 ;; Helpful function to blow up font for presentations
 (defun presentation-sized-font ()
