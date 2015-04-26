d172f6e72fb26e218292b019043506616467da1a
Danny Gratzer <danny.gratzer@gmail.com>
2015-04-26 09:04:36 -0400

    Added gotham theme

--- init/init-colors.el
+++ init/init-colors.el
@@ -1,12 +1,14 @@
 (require-package 'color-theme)
 (require-package 'monokai-theme)
 (require-package 'moe-theme)
+(require-package 'gotham-theme)
 
 (setq color-theme-is-global t)
 (color-theme-initialize)
 
-;(load-theme 'monokai t)
-(load-theme 'moe-dark t)
+;;(load-theme 'monokai t)
+;;(load-theme 'moe-dark t)
+(load-theme 'gotham t)
 
 ;; If we're living in th eterminal, we really don't want
 ;; a color theme.
