e5fec9a619a87e7c5c4371312fe01bafa7186ff1
Danny Gratzer <jozefg@cmu.edu>
2015-05-29 19:14:58 -0400

    More fiddling with ocaml config

--- init/init-ocaml.el
+++ init/init-ocaml.el
@@ -9,25 +9,24 @@
 
 (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
 (add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer)
-(add-hook 'tuareg-mode-hook 'merlin-mode)
-
-;; merlin
-(require-package 'merlin)
 
 (setq opam-share
       (substring (shell-command-to-string "opam config var share") 0 -1))
 
 (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
 
-(add-hook 'tuareg-mode-hook 'merlin-mode) ; Enable Merlin for ML buffers
-(set-face-background 'merlin-type-face "#88FF44")
+(setq ocamlbuild-command
+  (concat "ocamlbuild -use-ocamlfind"
+          " -pkgs sexplib.syntax,sexplib,core"
+          " -tags thread,principal"
+          " -syntax camlp4o "))
 
 ;; Avoid carpal tunnel
 (defun corebuild (buffer)
   (interactive (list (buffer-name)))
-  (compile (concat "corebuild "
+  (compile (concat ocamlbuild-command
                    (substring buffer 0 -3)
-                   ".cmx")))
+                   ".native")))
 
 (define-key tuareg-mode-map (kbd "C-c C-c") 'corebuild)
 
