20204285d35b51ea620ff26dc42b711fd595d0d0
Danny Gratzer <danny.gratzer@gmail.com>
2016-02-09 13:44:02 -0500

    Added support for paredit to elisp mode

--- init/init-elisp.el
+++ init/init-elisp.el
@@ -1,3 +1,7 @@
 (require-package 'flycheck-package)
 
+(require-package 'paredit)
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+(define-key paredit-mode-map (kbd "C-j") nil)
+
 (provide 'init-elisp)
