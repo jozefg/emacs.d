4a58b1f5dc18be79408afda655bab3a11cf92840
Danny Gratzer <danny.gratzer@gmail.com>
2018-09-20 11:12:23 +0200

    100 characters instead of 80

--- init/init-whitespace.el
+++ init/init-whitespace.el
@@ -14,7 +14,8 @@
       (add-hook 'before-save-hook 'delete-trailing-whitespace)
     (setq before-save-hook nil)))
 
-(setq whitespace-line-column 80) ; This is the default value, but I tweak a lot
+(setq whitespace-line-column 100) ; This is the default value, but I tweak a lot
+(setq-default fill-column whitespace-line-column)
 (setq whitespace-style '(face tabs lines-tail))
 
 (global-whitespace-mode 1) ; Judgement everywhere.
